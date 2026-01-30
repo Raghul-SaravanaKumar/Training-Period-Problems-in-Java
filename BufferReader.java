86e005ebe4e2283c65e098d025c78f75d219739e
Raghul18 <rexizzz1815@gmail.com>
2026-01-30 11:51:57 +0530

    This is the program to read the text in the other file using the BufferReader

---
+++ BufferReader.java
@@ -0,0 +1,14 @@
+package regex;
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class BufferReader {
+    public static void main(String[] args)throws Exception {
+       BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+       String line;
+
+       while((line=br.readLine())!=null){
+           System.out.println(line);
+       }
+       br.close();
+    }
+}
