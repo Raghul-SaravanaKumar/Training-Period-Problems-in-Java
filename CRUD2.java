709f475cb268386e2859372428bc67ba1fd74193
RAGHUL S <rexizzz1815@gmail.com>
2026-02-06 19:13:06 -1200

    Create CRUD2.java
    This is the program to create the CRUD operation using the BST and inserting the needed elements and we are going to search the elements present in it.

---
+++ CRUD2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class CRUD{
+static class Node{
+    int data;
+    Node left ,right;
+
+    Node(int data) {
+        this.data = data;
+    }
+    
+}
+static Node insert(Node root , int key){
+    if(root == null){
+        return new Node(key);
+    }
+
+    if(key < root.data){
+        root.left = insert(root.left, key);
+    }
+    else if(key > root.data){
+        root.right = insert(root.right, key);
+    }
+    return root;
+}
+static void inorder(Node root){
+    if(root == null)
+        return;
+    inorder(root.left);
+    System.out.print(root.data + " ");
+    inorder(root.right);
+}
+static boolean search(Node root, int key){
+    if (root == null)
+        return false;
+    if(key == root.data)
+        return true;
+    if(key < root.data)
+        return search(root.left, key);
+
+    return search(root.right, key);  
+}
+
+
+public static void main(String[] args) {
+    Node root = null;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the value of n: ");
+    int n = sc.nextInt();
+    int values [] = new int[n];
+    System.out.println("Enter the values: ");
+    for(int i=0;i<values.length;i++){
+        values[i] = sc.nextInt();
+    }
+    // int [] values = {50,40,20,30,10,70,90,80,60};
+    for(int val : values){
+        root  = insert(root, val);
+    }
+        System.out.print("BST Inorder Traversal: ");
+        inorder(root);
+        System.out.println("Search: " + search(root,90)); 
+}
+}
