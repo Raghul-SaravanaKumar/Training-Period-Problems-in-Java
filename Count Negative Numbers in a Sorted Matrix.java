e910df38a7ec607ad872dcd4ada2052806af4653
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-28 22:26:59 +0530

    Implement countNegatives method in Solution class

---
+++ Count Negative Numbers in a Sorted Matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public int countNegatives(int[][] grid) {
+
+        int count = 0;
+
+        for(int[] row : grid) {
+
+            for(int num : row) {
+
+                if(num < 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
