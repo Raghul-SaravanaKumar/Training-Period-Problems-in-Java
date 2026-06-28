a0b884c78037c90b7513563e07b1bcc1ead5cb6f
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-28 22:27:25 +0530

    Implement deepestLeavesSum method for binary tree

---
+++ Deepest Leaves Sum.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    int maxDepth = -1;
+    int sum = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+
+        dfs(root, 0);
+        return sum;
+    }
+
+    private void dfs(TreeNode node, int depth) {
+
+        if(node == null) {
+            return;
+        }
+
+        if(node.left == null && node.right == null) {
+
+            if(depth > maxDepth) {
+
+                maxDepth = depth;
+                sum = node.val;
+
+            } else if(depth == maxDepth) {
+
+                sum += node.val;
+            }
+        }
+
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
+}
