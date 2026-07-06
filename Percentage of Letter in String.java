66ce20761898b1c1443051853073506988d0d377
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-06 22:11:04 +0530

    Add method to calculate percentage of letter in string

---
+++ Percentage of Letter in String.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int percentageLetter(String s, char letter) {
+
+        int count = 0;
+
+        for(char c : s.toCharArray()) {
+
+            if(c == letter) {
+                count++;
+            }
+        }
+
+        return (count * 100) / s.length();
+    }
+}
