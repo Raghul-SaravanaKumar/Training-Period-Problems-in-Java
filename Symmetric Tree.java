38b94f24ab8d67950bc71177d717182418bbef41
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-07-23 22:10:36 +0530

    Create Symmetric Tree.java

---
+++ Symmetric Tree.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        return mirror(root.left, root.right);
+    }
+
+    private boolean mirror(TreeNode left, TreeNode right) {
+
+        if(left == null && right == null) {
+            return true;
+        }
+
+        if(left == null || right == null) {
+            return false;
+        }
+
+        return left.val == right.val &&
+               mirror(left.left, right.right) &&
+               mirror(left.right, right.left);
+    }
+}
